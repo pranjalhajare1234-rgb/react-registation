ed3805a2d0349635021acf019c83d5caf3f0069a
pranjal <pranjalhajare1234@gmail.com>
2026-04-03 16:19:20 +0530

    Add React project files and changes

---
+++ src/nav.js
@@ -0,0 +1,15 @@
+ import React from 'react';
+ import styled from 'styled-components';
+ import PropTypes from 'prop-types';
+ import navData from './navData';
+
+ const propTypes = {};
+ 
+ const defaultProps = {};
+ const nav = () => {
+    return <div></div>;
+ }
+ nav.propTypes = propTypes;
+ nav.defaultProps = defaultProps;
+ 
+ export default nav;
